35cad5690d7460f427ed4c20bc0177f2aa824947
ann <akostrik@e1r1p8.clusters.42paris.fr>
2022-11-30 22:28:01 +0100

    *

--- workspace/get_next_line.c
+++ workspace/get_next_line.c
@@ -6,39 +6,31 @@
 /*   By: akostrik <akostrik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 13:46:18 by akostrik          #+#    #+#             */
-/*   Updated: 2022/11/30 22:14:47 by akostrik         ###   ########.fr       */
+/*   Updated: 2022/11/30 22:26:13 by akostrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// Returns the line read from file or stdin / NULL nothing else to read / NULL error
-// An undefined behavior if the file fd changed since the last call whereas read() didn’t reach the end of file
+// Repeated calls to get_next_line() let the text file fd, one line at a time
+// Returns the line read from file, stdin / NULL nothing to read / NULL error
+// A new line -> to return the current line
+// Read as little as possible each time get_next_line() is called
+// File changed since the last call whereas read didn’t reach the EOF -> undef. behavior
+// Binary file -> undef. behavior
 
-// An undefined behavior when reading a binary file
+// Don’t read the whole file and then process each line
+// YES read malloc free
+// NO lseek() libft 
+// The helper functions in get_next_line_utils.c	
+// compiler -D BUFFER_SIZE=n for read
 // BUFFER_SIZE value is 9999? 10000000?
-// Read as little as possible each time get_next_line() is called. If you 
-// encounter a new line -> to return the current line.
-// Don’t read the whole file and then process each line.
-
-// read malloc free
-// lseek(), libft forbidden
-// all the helper functions in get_next_line_utils.c	
-
-// Repeated calls to get_next_line() let read the text file pointed to by 
-// the file descriptor, one line at a time
-
-// read files in get_next_line(), compiler -D BUFFER_SIZE=n, buffer size read()
-// The buffer size value will be modified by evaluators and the Moulinette
-
 // The buffer size and the line size can be of very different values
+// The buffer size will be modified by evaluators / Moulinette
 
 #include "get_next_line.h"
 
 char *get_next_line(int fd);
 {
-	size_t	i;
-	ssize_t	ret;
-
-	i = 0;
-	ret = 0;
-	return ((int)ret);
+	printf("get_next_line %d, buffer size = %d\n",fd);
+	// ssize_t read(int fildes, void *buf, size_t nbyte);
+	return ("abc");
 }
--- workspace/get_next_line.h
+++ workspace/get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: akostrik <akostrik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 12:24:00 by akostrik          #+#    #+#             */
-/*   Updated: 2022/11/30 22:15:03 by akostrik         ###   ########.fr       */
+/*   Updated: 2022/11/30 22:27:12 by akostrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
 
 char *get_next_line(int fd);
 
